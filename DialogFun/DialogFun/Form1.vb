a57f9588b6656ad0203e3351391c87a1e8f205af
lovercash <31711416+lovercash@users.noreply.github.com>
2017-11-03 09:35:57 -0400

    Second
    Just fooling around

--- DialogFun/DialogFun/Form1.vb
+++ DialogFun/DialogFun/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
-    Private Sub btnShowMessage_Click(sender As Object, e As EventArgs) Handles btnShowMessage.Click
-        MessageBox.Show("Hiya")
+    Private Sub BtnShowMessage_Click(sender As Object, e As EventArgs) Handles btnShowMessage.Click
+        MessageBox.Show("Hiya", "Caption", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk)
     End Sub
 End Class
