1c7d912be153e9b43c58740216cd06c863675b65
lovercash <31711416+lovercash@users.noreply.github.com>
2017-11-03 10:32:34 -0400

    Open-Save
    Playing with Open and Save Dialogs

--- DialogFun/DialogFun/Form1.Designer.vb
+++ DialogFun/DialogFun/Form1.Designer.vb
@@ -22,29 +22,254 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.btnShowMessage = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnSaveFile = New System.Windows.Forms.Button()
+        Me.txtSaveFile = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnFileToOpen = New System.Windows.Forms.Button()
+        Me.txtFileToOpen = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ofdData = New System.Windows.Forms.OpenFileDialog()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.sfdData = New System.Windows.Forms.SaveFileDialog()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnShowMessage
         '
-        Me.btnShowMessage.Location = New System.Drawing.Point(38, 29)
+        Me.btnShowMessage.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnShowMessage.Location = New System.Drawing.Point(103, 57)
         Me.btnShowMessage.Name = "btnShowMessage"
-        Me.btnShowMessage.Size = New System.Drawing.Size(142, 23)
+        Me.btnShowMessage.Size = New System.Drawing.Size(216, 23)
         Me.btnShowMessage.TabIndex = 0
         Me.btnShowMessage.Text = "Show Message"
         Me.btnShowMessage.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.Location = New System.Drawing.Point(346, 107)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.btnExit)
+        Me.Panel1.Controls.Add(Me.btnShowMessage)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(426, 135)
+        Me.Panel1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(72, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(290, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Message Box Botton Example"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.btnSaveFile)
+        Me.Panel2.Controls.Add(Me.txtSaveFile)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.btnFileToOpen)
+        Me.Panel2.Controls.Add(Me.txtFileToOpen)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.MenuStrip1)
+        Me.Panel2.Location = New System.Drawing.Point(12, 176)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(426, 170)
+        Me.Panel2.TabIndex = 3
+        '
+        'btnSaveFile
+        '
+        Me.btnSaveFile.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSaveFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSaveFile.Location = New System.Drawing.Point(332, 101)
+        Me.btnSaveFile.Name = "btnSaveFile"
+        Me.btnSaveFile.Size = New System.Drawing.Size(62, 23)
+        Me.btnSaveFile.TabIndex = 9
+        Me.btnSaveFile.Text = "&Save"
+        Me.btnSaveFile.UseVisualStyleBackColor = True
+        '
+        'txtSaveFile
+        '
+        Me.txtSaveFile.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSaveFile.Location = New System.Drawing.Point(103, 101)
+        Me.txtSaveFile.Name = "txtSaveFile"
+        Me.txtSaveFile.Size = New System.Drawing.Size(221, 20)
+        Me.txtSaveFile.TabIndex = 8
+        Me.txtSaveFile.Text = "SaveMe.txt"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(9, 100)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 18)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "File to Save"
+        '
+        'btnFileToOpen
+        '
+        Me.btnFileToOpen.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnFileToOpen.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFileToOpen.Location = New System.Drawing.Point(332, 72)
+        Me.btnFileToOpen.Name = "btnFileToOpen"
+        Me.btnFileToOpen.Size = New System.Drawing.Size(62, 23)
+        Me.btnFileToOpen.TabIndex = 6
+        Me.btnFileToOpen.Text = "&Open"
+        Me.btnFileToOpen.UseVisualStyleBackColor = True
+        '
+        'txtFileToOpen
+        '
+        Me.txtFileToOpen.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtFileToOpen.Location = New System.Drawing.Point(103, 72)
+        Me.txtFileToOpen.Name = "txtFileToOpen"
+        Me.txtFileToOpen.Size = New System.Drawing.Size(221, 20)
+        Me.txtFileToOpen.TabIndex = 5
+        Me.txtFileToOpen.Text = "OpenMe.txt"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(9, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 18)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "File to Open"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(84, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(267, 24)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Dialog File Control Example"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'ofdData
+        '
+        Me.ofdData.FileName = "OpenFileDialog1"
+        Me.ofdData.Filter = "Text Files | *.txt| Word Files | *.doc | Word Files | *.docx |  All Files |*.*"
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'sfdData
+        '
+        Me.sfdData.Filter = "Text Files | *.txt| Word Files | *.doc | Word Files | *.docx |  All Files |*.*"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(424, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.OpenToolStripMenuItem.Text = "&Open"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SaveToolStripMenuItem.Text = "&Save"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(586, 887)
-        Me.Controls.Add(Me.btnShowMessage)
+        Me.ClientSize = New System.Drawing.Size(450, 818)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents btnShowMessage As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnFileToOpen As Button
+    Friend WithEvents txtFileToOpen As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ofdData As OpenFileDialog
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents btnSaveFile As Button
+    Friend WithEvents txtSaveFile As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents sfdData As SaveFileDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
 End Class
--- DialogFun/DialogFun/Form1.vb
+++ DialogFun/DialogFun/Form1.vb
@@ -1,5 +1,31 @@
 ﻿Public Class Form1
     Private Sub BtnShowMessage_Click(sender As Object, e As EventArgs) Handles btnShowMessage.Click
-        MessageBox.Show("Hiya", "Caption", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk)
+        If MessageBox.Show("Hiya", "Yes or No", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk) = DialogResult.Yes Then
+            MessageBox.Show("You clicked Yes")
+        Else
+            MessageBox.Show("You clicked No")
+        End If
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        If MessageBox.Show("Are you sure you would like to exit the program?", "Confirm", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Application.Exit() 'Exiting the program
+        Else
+            MessageBox.Show("The program will not exit")
+        End If
+    End Sub
+
+    Private Sub BtnFileToOpen_Click(sender As Object, e As EventArgs) Handles btnFileToOpen.Click, OpenToolStripMenuItem.Click
+        ofdData.FileName = txtFileToOpen.Text
+        If ofdData.ShowDialog() = DialogResult.OK Then
+            txtFileToOpen.Text = ofdData.FileName
+        End If
+    End Sub
+
+    Private Sub BtnSaveFile_Click(sender As Object, e As EventArgs) Handles btnSaveFile.Click, SaveToolStripMenuItem.Click
+        sfdData.FileName = txtSaveFile.Text
+        If sfdData.ShowDialog() = DialogResult.OK Then
+            txtSaveFile.Text = sfdData.FileName
+        End If
     End Sub
 End Class
